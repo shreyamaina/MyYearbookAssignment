5c9b0eb21c5959409cd048abbb73284721f2dee9
shreyamaina <smainali@student.sjcny.edu>
2022-02-08 23:32:44 -0500

    Assignment 2

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myyearbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        txt = (TextView) findViewById(R.id.maintext);
+        btn = (Button) findViewById(R.id.button2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText("I can speak 3 languages");
+            }
+            })
+
+    ;}
 }
