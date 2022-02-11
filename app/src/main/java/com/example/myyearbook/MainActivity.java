00a69294677c32a7e4ce4a3e21ed861d5f67ff4e
shreyamaina <smainali@student.sjcny.edu>
2022-02-10 19:48:04 -0500

    Assignment 2

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +26,25 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 txt.setText("I can speak 3 languages");
             }
-            })
+            });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText("What's Next?");
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            class NextActivity {
+            }
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (getApplicationContext(),NextActivity.class);
+                startActivity(i);
+            }
+        })
 
     ;}
 }
---
+++ app/src/main/java/com/example/myyearbook/MyPlanAfterGraduation.java
@@ -0,0 +1,14 @@
+package com.example.myyearbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPlanAfterGraduation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_plan_after_graduation);
+    }
+}
