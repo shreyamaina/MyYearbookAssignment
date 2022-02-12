8b744a217f7fb656849e9b4b646475201437e864
shreyamaina <smainali@student.sjcny.edu>
2022-02-12 09:01:06 -0500

    Zach Pages Update

--- app/src/main/java/com/example/myyearbook/MyPlanAfterGraduation.java
+++ app/src/main/java/com/example/myyearbook/MyPlanAfterGraduation.java
@@ -2,13 +2,31 @@ package com.example.myyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MyPlanAfterGraduation extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_plan_after_graduation);
-    }
+
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            class NextActivity {
+            }
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent (getApplicationContext(),MainActivityZach.class);
+                startActivity(i);
+            }
+        })
+    ;}
 }
