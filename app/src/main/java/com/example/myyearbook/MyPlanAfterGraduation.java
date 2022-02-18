0848d46aad07158d65bb3929c4ceecf1716c01cd
shreyamaina <smainali@student.sjcny.edu>
2022-02-17 22:31:17 -0500

    Picture Error Update

--- app/src/main/java/com/example/myyearbook/MyPlanAfterGraduation.java
+++ app/src/main/java/com/example/myyearbook/MyPlanAfterGraduation.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 
 public class MyPlanAfterGraduation extends AppCompatActivity {
 
     Button btn2;
+    WebView weblink;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_plan_after_graduation);
 
+
         btn2 = (Button) findViewById(R.id.nextbutton);
+        weblink = findViewById(R.id.weblink);
+        WebSettings webSettings = weblink.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
 
 
